cd13aac712374f59827abe881c9fb8c19c489aa0
SMK <smarasan@gmail.com>
2023-01-25 10:21:38 +0530

    imagegen

---
+++ openai-python/imagegen.py
@@ -0,0 +1,14 @@
+import os
+import openai
+import sys
+import wget
+input_text=sys.argv[1]
+response = openai.Image.create(
+  prompt=input_text,
+  n=1,
+  size="1024x1024"
+)
+image_url = response['data'][0]['url']
+print(image_url)
+filename=wget.download(image_url,out="/tmp/images")
+print(filename)
